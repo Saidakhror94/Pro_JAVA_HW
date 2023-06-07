f6139742216059a5bf69f09002ed2249fdfba637
Saidakhror94 <124196846+Saidakhror94@users.noreply.github.com>
2023-06-07 21:48:50 +0200

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,8 +1,15 @@
 package Pro_JAVA_HW;
 
+//Напишите класс Person, который имеет следующие поля:
 
-public class Person implements Comparable<Person> {
-    private String name;
+
+// name (тип String) - имя человека;
+// age (тип int) - возраст человека.
+// Реализуйте интерфейс Comparable для класса Person, чтобы объекты Person могли быть сравниваемыми на основе возраста.
+//При сравнении двух объектов Person должно возвращаться отрицательное число, если первый объект младше второго, положительное число, 
+//если первый объект старше второго, и ноль, если объекты равны по возрасту.
+ public class Person implements Comparable<Person> {
+   private String name;
     private int age;
 
 
